97712ef1c16b44b441e38ffff33e124d024f51ed
Mutiara Botani <mutiarabotami2222@gmail.com>
2025-12-30 12:53:19 +0700

    sim_ppdb

--- app/views/siswa/cetak_formulir.php
+++ app/views/siswa/cetak_formulir.php
@@ -21,35 +21,85 @@ $base = Config::base_url();
 
 <hr>
 
+<!-- ================= DATA SISWA ================= -->
 <h4>DATA SISWA</h4>
 <table class="border">
-<tr><td class="label">NISN</td><td><?= $data["siswa"]["nisn"] ?></td></tr>
-<tr><td class="label">Nama Lengkap</td><td><?= $data["siswa"]["nama_lengkap"] ?></td></tr>
+<tr>
+    <td class="label">NISN</td>
+    <td><?= $data["siswa"]["nisn"] ?? "-" ?></td>
+</tr>
+<tr>
+    <td class="label">Nama Lengkap</td>
+    <td><?= $data["siswa"]["nama_lengkap"] ?? "-" ?></td>
+</tr>
 <tr>
     <td class="label">Tempat, Tanggal Lahir</td>
-    <td><?= $data["siswa"]["tempat_lahir"] ?>, <?= $data["siswa"]["tanggal_lahir"] ?></td>
+    <td>
+        <?= $data["siswa"]["tempat_lahir"] ?? "-" ?>,
+        <?= $data["siswa"]["tanggal_lahir"] ?? "-" ?>
+    </td>
 </tr>
-<tr><td class="label">Jenis Kelamin</td><td><?= $data["siswa"]["jenis_kelamin"] ?></td></tr>
-<tr><td class="label">Agama</td><td><?= $data["siswa"]["agama"] ?></td></tr>
-<tr><td class="label">Alamat</td><td><?= $data["siswa"]["alamat"] ?></td></tr>
+<tr>
+    <td class="label">Jenis Kelamin</td>
+    <td><?= $data["siswa"]["jenis_kelamin"] ?? "-" ?></td>
+</tr>
+<tr>
+    <td class="label">Agama</td>
+    <td><?= $data["siswa"]["agama"] ?? "-" ?></td>
+</tr>
+<tr>
+    <td class="label">Alamat</td>
+    <td><?= $data["siswa"]["alamat"] ?? "-" ?></td>
+</tr>
 </table>
 
+<!-- ================= DATA ORANG TUA ================= -->
 <h4>DATA ORANG TUA</h4>
 <table class="border">
-<tr><td class="label">Nama Ayah</td><td><?= $data["ortu"]["nama_ayah"] ?></td></tr>
-<tr><td class="label">Pekerjaan Ayah</td><td><?= $data["ortu"]["pekerjaan_ayah"] ?></td></tr>
-<tr><td class="label">Nama Ibu</td><td><?= $data["ortu"]["nama_ibu"] ?></td></tr>
-<tr><td class="label">Pekerjaan Ibu</td><td><?= $data["ortu"]["pekerjaan_ibu"] ?></td></tr>
-<tr><td class="label">Alamat Orang Tua</td><td><?= $data["ortu"]["alamat_rumah_ayah"] ?></td></tr>
+
+<tr>
+    <td class="label">Nama Ayah</td>
+    <td><?= $data["ortu"]["ayah"]["nama_orang_tua"] ?? "-" ?></td>
+</tr>
+<tr>
+    <td class="label">Pekerjaan Ayah</td>
+    <td><?= $data["ortu"]["ayah"]["pekerjaan"] ?? "-" ?></td>
+</tr>
+
+<tr>
+    <td class="label">Nama Ibu</td>
+    <td><?= $data["ortu"]["ibu"]["nama_orang_tua"] ?? "-" ?></td>
+</tr>
+<tr>
+    <td class="label">Pekerjaan Ibu</td>
+    <td><?= $data["ortu"]["ibu"]["pekerjaan"] ?? "-" ?></td>
+</tr>
+
+<tr>
+    <td class="label">Alamat Orang Tua</td>
+    <td><?= $data["ortu"]["ayah"]["alamat_rumah"] ?? "-" ?></td>
+</tr>
+
 </table>
 
+<!-- ================= DATA WALI ================= -->
 <h4>DATA WALI</h4>
 <table class="border">
-<tr><td class="label">Nama Wali</td><td><?= $data["wali"]["nama_wali"] ?? "-" ?></td></tr>
-<tr><td class="label">Pekerjaan</td><td><?= $data["wali"]["pekerjaan_wali"] ?? "-" ?></td></tr>
-<tr><td class="label">No HP</td><td><?= $data["wali"]["hp_wali"] ?? "-" ?></td></tr>
+<tr>
+    <td class="label">Nama Wali</td>
+    <td><?= $data["wali"]["nama_orang_tua"] ?? "-" ?></td>
+</tr>
+<tr>
+    <td class="label">Pekerjaan</td>
+    <td><?= $data["wali"]["pekerjaan"] ?? "-" ?></td>
+</tr>
+<tr>
+    <td class="label">No HP</td>
+    <td><?= $data["wali"]["nomor_hp"] ?? "-" ?></td>
+</tr>
 </table>
 
+<!-- ================= TANDA TANGAN ================= -->
 <table width="100%" class="ttd">
 <tr>
 <td width="60%"></td>
