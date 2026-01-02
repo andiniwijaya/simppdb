18aa869b031c0d79a0454ca9c1ecf984adf691c8
Mutiara Botani <mutiarabotami2222@gmail.com>
2026-01-02 18:11:18 +0700

    sim_ppdb

--- app/views/home.php
+++ app/views/home.php
@@ -9,13 +9,10 @@ $base = Config::base_url();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SMP PGRI ARJASARI | PPDB Online 2026/2027</title>
 
-    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
 
-    <!-- Custom CSS -->
     <link rel="stylesheet" href="<?= $base ?>/public/assets/css/home.css">
 </head>
 
