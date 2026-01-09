4cfc0b8ec63967525b2ef0c2d79f776ff0459e58
Mutiara Botani <mutiarabotami2222@gmail.com>
2026-01-09 15:41:43 +0700

    sim_ppdb

--- app/views/admin/ppdb_detail.php
+++ app/views/admin/ppdb_detail.php
@@ -1,14 +1,29 @@
-<link rel="stylesheet" href="<?= $base ?>/public/assets/css/ppdb_detail.css">
+<?php
+$base = Config::base_url();
+?>
 
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Detail PPDB</title>
+
+    <link rel="stylesheet" href="<?= $base ?>/public/assets/css/admin.css">
+    <link rel="stylesheet" href="<?= $base ?>/public/assets/css/ppdb_detail.css">
+</head>
+<body>
 
 <h3>Detail PPDB</h3>
 
 <table class="table">
-<tr><th>Nama</th><td><?= $data['nama_lengkap'] ?></td></tr>
-<tr><th>NISN</th><td><?= $data['nisn'] ?></td></tr>
-<tr><th>Asal Sekolah</th><td><?= $data['asal_sekolah'] ?></td></tr>
-<tr><th>Status</th><td><?= $data['status_data'] ?></td></tr>
-<tr><th>Tanggal Daftar</th><td><?= $data['tanggal_daftar'] ?></td></tr>
+    <tr><th>Nama</th><td><?= $data['nama_lengkap'] ?></td></tr>
+    <tr><th>NISN</th><td><?= $data['nisn'] ?></td></tr>
+    <tr><th>Asal Sekolah</th><td><?= $data['asal_sekolah'] ?></td></tr>
+    <tr><th>Status</th><td><?= $data['status_data'] ?></td></tr>
+    <tr><th>Tanggal Daftar</th><td><?= $data['tanggal_daftar'] ?></td></tr>
 </table>
 
-<a href="/admin/ppdb" class="btn btn-secondary">Kembali</a>
+<a href="<?= $base ?>/admin/ppdb" class="btn btn-secondary">Kembali</a>
+
+</body>
+</html>
