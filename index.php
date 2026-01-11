c76eeb4ebe97e8764e72127f0a32c472d9ce793a
Mutiara Botani <mutiarabotami2222@gmail.com>
2026-01-11 08:52:24 +0700

    sim_ppdb

--- index.php
+++ index.php
@@ -26,6 +26,8 @@ $router->post('/dashboard/user/store', 'DashboardController@storeUser');
 
 $router->get('/dashboard/user/edit', 'DashboardController@editUser');
 $router->post('/dashboard/user/update', 'DashboardController@updateUser');
+$router->post('/dashboard/updateUser', 'DashboardController@updateUser');
+
 
 $router->get('/dashboard/user/delete', 'DashboardController@deleteUser');
 
